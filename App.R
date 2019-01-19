ceceeeb7f080bb649cd34845d8bbef4eeaf361fe
Rodrick Sarmento <Sarmentor@users.noreply.github.com>
2019-01-19 18:45:02 +0000

    Update App.R

--- App.R
+++ App.R
@@ -4,8 +4,12 @@ library(shinythemes)
 library(Rtts)
 library(htm2txt)
 
+my.dir <- getwd()
+setwd(dir=my.dir)
+
 ui <- fluidPage(theme = shinytheme("slate"),
-                textInput("n", "Input:"),
+                selectInput(inputId="lang", label="Select Language:",choices= c("EN","PT"), selected="EN"),
+                textInput("n", "Input Search:"),
                 actionButton("Run", "Click Me"),
                 verbatimTextOutput("nText")
                 
@@ -14,21 +18,57 @@ ui <- fluidPage(theme = shinytheme("slate"),
 server <- function(input, output, session){
   ntext <- eventReactive(input$Run, {
     inp = input$n
-    url1 <- 'https://en.wikipedia.org/wiki'
-    urlNew = paste(url1, inp,sep = '/')
-    text <- gettxt(urlNew)
-    new_text = substr(text, start = 1, stop = 100)
-    tts_ITRI(new_text, speaker = "ENG_Bob",destfile="www/temp.mp3")
-    text
+    lang = input$lang
+    
+    if(lang=="EN"){
+      url1.en <- 'https://en.wikipedia.org/wiki'
+      urlNew.en = paste(url1.en, inp,sep = '/')
+      text.en <- gettxt(urlNew.en)
+      text.en
+      
+      phrases.to.read <- strsplit(text.en, "[\r\n]")[[1]]
+      for (i in 1:length(phrases.to.read)){
+        
+        system("cmd.exe", input = paste('espeak -s150 -ven "',phrases.to.read[i],'"', sep=""))
+        
+      } 
+      
+      
+    }
+    
+    if(input$lang=="PT"){
+      url1.pt <- 'https://pt.wikipedia.org/wiki'
+      urlNew.pt = paste(url1.pt, inp,sep = '/')
+      text.pt <- gettxt(urlNew.pt)
+      text.pt
+      
+      phrases.to.read <- strsplit(text.pt, "[\r\n]")[[1]]
+      
+      for (i in 1:length(phrases.to.read)){
+        
+        system("cmd.exe", input = paste('espeak -s140 -vpt-pt -k -20 "',phrases.to.read[i],'"', sep=""))
+        
+      } 
+      
+    }
+    
+        #com tts_ITRI
+    #text.new <- gsub("[\r\n]", ". ", text)
+    #browser()
+    #text.new = substr(text.new, start = 1, stop = 1000)
+    
+    #tts_ITRI(new_text, speaker = "ENG_Bob",destfile="temp.mp3")
+    #tts_ITRI(text, speaker = "ENG_Bob",destfile="temp.mp3")
+    
   })
   
-  observeEvent(input$Run, {
-    insertUI(selector = "#Run",
-             where = "afterEnd",
-             ui = tags$audio(src = "temp.mp3", type = "audio/mp3", autoplay = NA, controls = NA, style="display:none;"),
-             immediate = FALSE
-    )
-  })
+  #observeEvent(input$Run, {
+  #  insertUI(selector = "#Run",
+  #           where = "afterEnd",
+  #           ui = tags$audio(src = "temp.mp3", type = "audio/mp3", autoplay = NA, controls = NA, style="display:none;"),
+  #           immediate = FALSE
+  #  )
+  #})
   
   
   output$nText <- renderText({
